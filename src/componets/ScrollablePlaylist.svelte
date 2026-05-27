75325adb55d307a31dee39daace5fd530938a89f
ReidS28 <strawj3434@gmail.com>
2026-05-27 18:08:19 -0400

    blank cover

--- src/componets/ScrollablePlaylist.svelte
+++ src/componets/ScrollablePlaylist.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { getPlaylistItems } from "../lib/api.svelte";
 	import { formatDurationMs } from "../lib/helper.svelte";
+	import type { ActionReturn } from "svelte/action";
 	import TrackBar from "./TrackBar.svelte";
 	let { data, class: className } = $props();
 
@@ -28,17 +29,17 @@
 	class="flex flex-col gap-1 bg-[#212121] rounded-3xl border-6 border-sp-green w-full h-full overflow-x-hidden overflow-y-auto scrollbar-thumb-sp-green {className}"
 >
 	<div class="flex flex-col">
-		<div class="flex flex-row p-2 gap-2 h-40 w-full shrink-0 bg-red-500">
-			{#if data?.images?.[0]?.url}
-				<img
-					class="h-full w-auto aspect-square rounded-2xl"
-					src={data?.images?.[0]?.url || "./assets/covers/Cold Space"}
-					alt="Album Cover"
-				/>
-			{/if}
-			<div class="flex flex-col h-full w-full bg-teal-500">
-				<div class="w-full grow text-5xl primaryText bg-orange-500">
-					{data?.name || "EEE"}
+		<div class="flex flex-row p-2 gap-2 h-40 w-full shrink-0 bg-red-500-">
+			<img
+				class="h-full w-auto aspect-square rounded-2xl"
+				src={data?.images?.[0]?.url || ""}
+				alt="Album Cover"
+			/>
+			<div class="flex flex-col h-full w-full bg-teal-500-">
+				<div
+					class="w-full grow text-5xl primaryText overflow-hidden break-all line-clamp-3 bg-orange-500"
+				>
+					{data?.name || ""}
 				</div>
 				<span class="secondaryText">{data?.items?.total || ""} tracks</span>
 				<span class="secondaryText"
